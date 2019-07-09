868bc1f0e02833bddf7d53646059d51310281567
mtorre <daniel.torre@mercadolibre.com>
2019-07-09 17:29:03 -0300

    Updates

---
+++ precache-manifest.c38b91fb07c6b943935563d185b30ab8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be65aff506b210fd470e69b189ff3a0",
+    "url": "/poc-infinite-scroll/index.html"
+  },
+  {
+    "revision": "7e6c615fd6855884637a",
+    "url": "/poc-infinite-scroll/static/css/main.028d9f1e.chunk.css"
+  },
+  {
+    "revision": "1ed446824f5e6dc7640b",
+    "url": "/poc-infinite-scroll/static/js/2.fc609788.chunk.js"
+  },
+  {
+    "revision": "7e6c615fd6855884637a",
+    "url": "/poc-infinite-scroll/static/js/main.e7add686.chunk.js"
+  },
+  {
+    "revision": "d4cb09bc26d70a2cc808",
+    "url": "/poc-infinite-scroll/static/js/runtime~main.438fc142.js"
+  }
+]);
